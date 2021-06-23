ab92bcf10f41085f008a0316786c093fd42ff6d6
Laura Tissi <laura.tracierra@gmail.com>
2021-06-22 21:41:44 -0300

    validaçao do sign up e insere no banco de dados,falta testar

--- src/server.js
+++ src/server.js
@@ -1,8 +1,58 @@
 import express from 'express';
+import Joi from 'joi';
+import bcrypt from 'bcrypt';
 import connection from './database/database.js';
 
 const app = express();
 app.use(express.json());
+
+app.post("/sign-up", async (req, res) => {
+  const { name, email, password, repeatPassword } = req.body;
+
+  const schema = Joi.object({
+    name: Joi.string().alphanum().min(1).required(),
+    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] }}).required(),
+    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{6,30}$')).required(),
+    repeatPassword: Joi.ref('password').required()
+  })
+
+  schema.validate({
+    name: name, 
+    email: email, 
+    password: password, 
+    repeatPassword: repeatPassword
+  })
+
+  if(error){
+    return res.sendStatus(400);
+  }
+
+  try{
+    const user = await connection.query(`
+      SELECT * FROM users
+      WHERE email = $1
+    `, [email]);
+
+    const validateUser = user.rows[0];
+    if(validateUser){
+      return res.sendStatus(400);
+    }
+
+    const hash = bcrypt.hashSync(password, 12);
+
+    await connection.query(`
+      INSERT INTO users
+      (name, email, password)
+      VALUES ($1, $2, $3)
+    `, [name, email, hash])
+
+    res.sendStatus(200);
+  }
+  catch(e){
+    console.log(e);
+    res.sendStatus(400);
+  }
+})
 
 app.listen(4000, () => {
     console.log('Server is listening on port 4000.');
