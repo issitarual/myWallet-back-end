d7b0c0549ba59dc1920663c96bfdbda3a8d6c008
Laura Tissi <laura.tracierra@gmail.com>
2021-06-24 10:47:51 -0300

    rotas do historico sem testar

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import bcrypt from 'bcrypt';
 import connection from './database/database.js';
 import cors from 'cors';
 import { v4 as uuid } from 'uuid';
+import dayjs from 'dayjs';
 
 const app = express();
 app.use(express.json());
@@ -102,11 +103,87 @@ app.post("/sign-in", async (req, res) => {
   };
 });
 
-app.get("/historic", async (req, res) => {
+app.post("/finances", async (req, res) => {
+  const authorization = req.headers['authorization'];
+  const { value, description, event_type } = req.body;
+  const token = authorization?.replace('Bearer ', "");
+
+  if(!token) return sendStatus(401);
+
+  const schema = Joi.object({
+    value: Joi.number().min(1).required(),
+    description: Joi.string().alphanum().pattern(/[a-zA-Z0-9]/).min(1).required()
+  })
+
+  const { error, value } = schema.validate({
+    email: email, 
+    password: password, 
+  })
+
+  if(error) return res.sendStatus(400);
+
+  const date = dayjs();
+  try{
+    const result = await connection.query(`
+      SELECT users.*, categories.id AS "categoryId"
+      FROM sessions
+      JOIN users
+      ON sessions."userId" = users.id
+      JOIN categories
+      ON categories.name = $1
+      WHERE sessions.token = $2
+    `, [event_type, token]);
+
+    const user = result.rows[0];
+    if(user && user.categoryId){
+      await connection.query(`
+        INSERT INTO financial_events
+        (valor, description, "categoryId", "userId", created_at)
+        VALUES ($1, $2, $3, $4,$5)
+      `[value, description, user.categoryId, user.id, date]);
+      res.sendStatus(201);
+    }
+    else{
+      res.sendStatus(401);
+    }
+    }
+  catch{
+    res.sendStatus(400);
+  };
+});
+
+app.get("/finances", async (req, res) => {
   const authorization = req.headers['authorization'];
   const token = authorization?.replace('Bearer ', "");
 
   if(!token) return sendStatus(401);
+
+  try{
+    const result = await connection.query(`
+      SELECT users.* FROM sessions
+      JOIN users
+      ON sessions."userId" = users.id
+      WHERE sessions.token = $1
+`   , [token]);
+    const user = result.rows[0];
+
+    if(user){
+      const historic = await connection.query(`
+        SELECT financial_events.created_at, financial_events.description, financial_events.valor, categories.name
+        FROM sessions   
+        JOIN financial_events
+        ON financial_events."userId" = sessions."userId"
+        JOIN categories
+        ON categories.id = financial_events."categoryId"
+        WHERE sessions.token = $1
+      `, [token]);
+
+    res.send(historic.rows);
+    }
+  }
+  catch{
+    res.sendStatus(400);
+  }
 
 });
 
